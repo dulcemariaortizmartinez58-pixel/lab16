1e9bb6ebd0d97b76fb7f7c811fb246bbbe1111e2
dulcemariaortizmartinez58-pixel <dulcemariaortizmartinez58@gmail.com>
2025-11-27 04:57:19 -0500

    g7

---
+++ script.js
@@ -0,0 +1,16 @@
+function validarFormulario(){
+    let nombre=document.getElementById("nombre").Value;
+    let email=document.getElementById("email").Value;
+    let imagen=document.getElementById("imagen")Value;
+    if(nombre=="" || email=="" || imagen=="" ){
+        alert("Todos los campos son obligatorios...");
+        return false;
+    }
+}
+return true;
+}
+function mostrarImagen(event){
+    let imagen=document.getElementById('ver-imagen');
+    imagen.src=URL.createObjectURL(event.target.files[0]);
+
+}
